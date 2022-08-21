78201434db24bbb0c206bc60a5907223522aa045
Thao <t.t.nguyen@live.de>
2022-08-21 17:16:54 +0200

    fix redis issues

--- src/main/java/com/kbertv/warehouse/model/PlanetarySystem.java
+++ src/main/java/com/kbertv/warehouse/model/PlanetarySystem.java
@@ -6,6 +6,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -13,7 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "products")
-public class PlanetarySystem {
+public class PlanetarySystem implements Serializable {
 
     @CsvBindByName
     @Id
