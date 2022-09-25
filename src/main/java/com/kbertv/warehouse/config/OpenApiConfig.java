63d6046c9e25177ff4e93a9d8844a2936406835a
nixkleber <s0569066@htw-berlin.de>
2022-09-25 17:02:24 +0200

    added openApi / Swagger UI

---
+++ src/main/java/com/kbertv/warehouse/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.kbertv.warehouse.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Warehouse API").description(
+                        "Provides the functionality to access and store inventory information to a warehouse.").version("v1"));
+    }
+
+}
